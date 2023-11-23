ec2355272b37cca3ecb87535231958365d66bfb8
reembadawi <116513774+reembadawi@users.noreply.github.com>
2023-11-23 14:50:14 +0300

    Add files via upload

---
+++ info.sql
@@ -0,0 +1,7 @@
+CREATE TABLE info (
+    user_id INT PRIMARY KEY IDENTITY(1,1),
+    atu_id INT UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    userName VARCHAR(50) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
